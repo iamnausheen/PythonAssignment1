5fd3b041bca9457e24432eaa0015e73cfc524898
iamnausheen <152173229+iamnausheen@users.noreply.github.com>
2024-06-16 15:58:13 +0530

    Update Program 14.py

--- Program 14.py
+++ Program 14.py
@@ -1,1 +1,13 @@
+# Prompt user for multi-line input
+lines = []
+
+while (True):
+    line = input()
+    if line:
+        lines.append(line)
+    else:
+        break
+# Print multi line input
+for i in lines:
+    print(i)
 
