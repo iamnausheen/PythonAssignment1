401f5d57ee8eff6dcda5cbe32cbd60e8b4cd2fa3
iamnausheen <152173229+iamnausheen@users.noreply.github.com>
2024-06-16 15:23:00 +0530

    Add files via upload

---
+++ Program 1.py
@@ -0,0 +1,11 @@
+# Prompting user for 2 numbers
+
+num_1 = int(input("Enter an integer: "))
+num_2 = int(input("Enter an integer: "))
+
+# Calculating sum
+
+num_3 = num_1 + num_2
+
+#Printing the sum
+print("Sum: ", num_3)
